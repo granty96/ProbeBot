e9d40b07da474996fa13e71a644b62c701bcc2f6
granty96 <122406728+granty96@users.noreply.github.com>
2024-04-12 15:06:40 -0600

    Finished measurment bot

--- ProbeBot/probebot/edge_detection.py
+++ ProbeBot/probebot/edge_detection.py
@@ -1,9 +1,13 @@
 import cv2
 import numpy as np
 from skimage import measure
+from scipy.optimize import minimize
+from scipy.interpolate import UnivariateSpline
 from sklearn.ensemble import RandomForestClassifier
 import pytesseract
 from pytesseract import Output
+from scipy.optimize import brentq
+from matplotlib import *
 import re
 from PIL import Image
 
@@ -22,86 +26,192 @@ def extract_scale_from_info_bar(image) -> float:
     scale_text = pytesseract.image_to_string(thresholded, config='--psm 6').strip()
     
     scale_text = float(re.sub(r'\D', '', scale_text))
-    print(scale_text)
     
     return scale_text
 
 
-image = cv2.imread('probebot/images/3630_005.tif', cv2.IMREAD_COLOR)
+image = cv2.imread('probebot/ProbeImages/4932_007.tif', cv2.IMREAD_COLOR)
 
 
 if(image is None):
     print("null image")
     
 roi = image[:-60, 0:607]
+roicopy = roi.copy()
 
 
-
+def solve_for_y(poly_coeffs, y):
+    pc = poly_coeffs.copy()
+    pc[-1] -= y
+    return np.roots(pc).real
 
 if roi is None:
     print("Error: Failed to load the ROI.")
 else:
     scale = extract_scale_from_info_bar(image)
-    if scale and scale == 300000.0:
         
-        nm_per_pixel = 200.0/81.0;
-        image_blurred_noHSV = cv2.GaussianBlur(roi, (5, 5), 0)
-        image_blurred = cv2.cvtColor(image_blurred_noHSV, cv2.COLOR_BGR2HSV)
+    nm_per_pixel = 200.0/81.0;
+    image_blurred_noHSV = cv2.GaussianBlur(roi, (5, 5), 0)
+    image_blurred = cv2.cvtColor(image_blurred_noHSV, cv2.COLOR_BGR2HSV)
+    
+    lower = np.array([0,0,0], dtype="uint8")
+    upper = np.array([180,255,40], dtype="uint8")
+    
+    mask = cv2.inRange(image_blurred, lower, upper)
+    mask = 255-mask
+    
+    image_blurred = mask     
+    
+    
+    # Edge Detection
+    edges = cv2.Canny(image_blurred, 30, 150)
+
+    # Find contours
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+
+    
+    
+    contour_points = [tuple(point[0]) for contour in contours for point in contour]
+    
+    
+    
+    x = [point[0] for point in contour_points]
+    y = [point[1] for point in contour_points]
+    
+    
+    fit =  np.polyfit(x,y,15)
+    approximationFunction = np.poly1d(fit)
+    
+    height, width, channels = roi.shape
+    
+    for i in range(width):
+        cv2.circle(roi, (i, int(approximationFunction(i))), 2, (255,255,0), -1 )
+    
+    
+    rootsX = np.array([root for root in approximationFunction.deriv().r.real if root > 0 ])
+
+    rootsY = np.array([root for root in approximationFunction(rootsX)])
+    
+    minY = min(rootsY)
+    # xy = np.c_[rootsX, rootsY].
+    roots =list(zip(rootsX, rootsY))
+    
+    criticalPoint = (0,minY)
+    
+    for root in roots:
+        if root[1] == minY:
+            criticalPoint = (int(root[0]), int(root[1]))
+            break
+    
+    # for root in roots:
+    #     cv2.circle(roi, (int(root[0]), int(root[1])), 5, (0,0,255), -1)
+           
+    # for i in range(width):
+    #     cv2.circle(roi, (i, 294), 2, (255,255,255), -1)
+    
+    
+    print(f"cp: {criticalPoint}")
+    
+    
+    cv2.circle(roi, criticalPoint, 5, (0,0,255), -1)
+    
+    left_partition = [point for point in contour_points if point[0] < criticalPoint[0]]
+    right_partition = [point for point in contour_points if point[0] > criticalPoint[0]]
+    
+    
+   
+    # Loop over each contour
+    for point in left_partition:
         
-        lower = np.array([0,0,0], dtype="uint8")
-        upper = np.array([180,255,40], dtype="uint8")
         
-        mask = cv2.inRange(image_blurred, lower, upper)
-        mask = 255-mask
-       
-        image_blurred = mask        
-       # Edge Detection
-        edges = cv2.Canny(image_blurred, 30, 150)
-
-        # Find contours
-        contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        # Find the contour with the largest area (probe tip)
-        largest_contour = max(contours, key=cv2.contourArea)
-
-        print(largest_contour)
-        # Approximate the contour with a polygon using the Douglas-Peucker algorithm
-        epsilon = 0.01 * cv2.arcLength(largest_contour, True)  # Adjust epsilon as needed
-        approx = cv2.approxPolyDP(largest_contour, epsilon, True)
-
-        # Sort the vertices based on their y-coordinates
-        sorted_vertices = sorted(approx[:, 0], key=lambda x: x[1])
-        pt1 = sorted_vertices[0]
-
+        color = (0,255,0)
         
-        #select the next vertex in the line
-        for vertex in sorted_vertices:
-            print(abs(vertex[0] - pt1[0]))
-            if(abs(vertex[0] - pt1[0]) > 10):
-                pt2 = vertex
-                break
+        cv2.circle(roi, point, 3, color, -1)
             
+    # Loop over each contour
+    for point in right_partition:
+        color = (255,0,0)
+        cv2.circle(roi, point, 4, color, -1)
+    
 
-        # Draw the lines connecting the selected vertices on the image
-          # Convert to color image for visualization
-        cv2.line(roi, tuple(pt1), tuple(pt2), (0, 255, 0), thickness=2)  # Draw the line in green with thickness 2
-        # cv2.line(line_image, tuple(pt1), tuple(pt3), (255, 0, 0), thickness=2)
-        # cv2.line(line_image, tuple(pt1), tuple(pt4), (0,0,255), thickness=2)
+    left_partition_x = [point[0] for point in left_partition]
+    left_partition_y = [point[1] for point in left_partition]
+    
+    left_partition_fit = np.polyfit(left_partition_x, left_partition_y, 1)
+    left_partition_fit_function = np.poly1d(left_partition_fit)
+    
+    right_partition_x = [point[0] for point in right_partition]
+    right_partition_y = [point[1] for point in right_partition]
+    
+    right_partition_fit = np.polyfit(right_partition_x, right_partition_y, 1)
+    right_partition_fit_function = np.poly1d(right_partition_fit)
+    
+    for i in range(width):
+        cv2.circle(roi, (i, int(left_partition_fit_function(i))), 2, (0,0,255), -1)
+    
+    for i in range(width):
+        cv2.circle(roi, (i, int(right_partition_fit_function(i))), 2, (0,0,255), -1)
+    
+    highestPoint = min(contour_points, key=lambda point: point[1])
+    
+    cv2.circle(roi, highestPoint, 5, (0,0,255), -1)
+    
+    print()
+    
+    left_roots = solve_for_y(left_partition_fit, highestPoint[1])
+    left_roots_distance_from_cp = {}
+    
+    print(f"left roots: {left_roots}")
+    
+    for root in left_roots:
+        left_roots_distance_from_cp[root] = (np.abs(criticalPoint[0] - root))
+    
+    left_measurement_point_x = int(min(left_roots_distance_from_cp, key=left_roots_distance_from_cp.get))
+    
+    print(left_measurement_point_x)
+    
+    right_roots = solve_for_y(right_partition_fit, highestPoint[1])
+    right_roots_distance_from_cp = {}
+    
+    for root in right_roots:
+        right_roots_distance_from_cp[root] = np.abs(criticalPoint[0] - root)
+    
+    right_measurement_point_x = int(min(right_roots_distance_from_cp, key=right_roots_distance_from_cp.get))
+    
+    left_measurement_point = (left_measurement_point_x, highestPoint[1])
+    right_measurement_point = (right_measurement_point_x, highestPoint[1])
+    
+    cv2.line(roicopy, left_measurement_point, right_measurement_point, (0,255,255), 2)
+    cv2.line(roi, left_measurement_point, right_measurement_point, (0,255,255), 2)
+    
+    diameter = abs(right_measurement_point_x - left_measurement_point_x)
+    
+    print(f"diameter : {diameter}")
 
-        # Display the image with the drawn lines
-        cv2.imshow('Image with Lines (Probe Tip)', roi)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        
-        # result_image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-        # cv2.circle(result_image, (int(x), int(y)), int(radius), (0,255,0), 2)
-        
-        # radius_nm = radius*nm_per_pixel
-
-        # print(str(radius_nm) + " nm")
-
-        # cv2.imshow('Result', result_image)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
+    tip_radius = diameter/2
+    
+    print(f"radius: {tip_radius}")
+    
+    result =""
+    
+    if (tip_radius > 0 and tip_radius <= 10):
+        result = "Sub 10"
+    elif (tip_radius > 10 and tip_radius <= 20):
+        result = "Sub 20"
+    elif (tip_radius > 20 and tip_radius <= 40):
+        result = "Sub 40"
     else:
-        print("No scale found")
+        result = str(int(tip_radius))
+   
+   
+    roiFinal = np.concatenate((roi, roicopy), axis=1)
+    
+    cv2.imshow(result, roiFinal)
+    # cv2.imshow(result, roi)
+       
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+    
 
